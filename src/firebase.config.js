580e5bbd20a5c4849de1b5909ff613603ba4472e
ashraful amin <ashrafulamin654@gmail.com>
2023-04-19 08:25:43 +0600

    61-1 Module Introduction firebase setup and git clone

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuJ8QXBu8Zx8XgP8rr1sOzri5URJCFxN4",
+  authDomain: "ema-john-firebase-auth-298ab.firebaseapp.com",
+  projectId: "ema-john-firebase-auth-298ab",
+  storageBucket: "ema-john-firebase-auth-298ab.appspot.com",
+  messagingSenderId: "445955249511",
+  appId: "1:445955249511:web:c18740b52bdef5e8bacbc2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
