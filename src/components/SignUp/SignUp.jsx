95a0fe23f4f32a84aea4854250aa9ffe34b00cec
ashraful amin <ashrafulamin654@gmail.com>
2023-04-19 20:50:42 +0600

    61-2 Create Login component and style Login form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import './SignUp.css';
 const SignUp = () => {
     return (
-        <div>
-            <h2>this is sign up</h2>
+        <div className='form-container'>
+        <h2 className='form-title'>Sign Up</h2>
+    <form>
+        <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input type="email" name='email' id='' required />
         </div>
+        <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" name='password' id='' required />
+        </div>
+        <div className="form-control">
+            <label htmlFor="password">Confirm Password</label>
+            <input type="password" name='confirm' id='' required />
+        </div>
+        <button className="btn-submit" type='submit' value='login'>Sign Up</button>
+    </form>
+    </div>
     );
 };
 
